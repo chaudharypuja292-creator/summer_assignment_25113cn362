fe20882998e384aedfa20100292fd4bf7447b289
chaudharypuja-creator <chaudharypuja292@gmail.com>
2026-06-08 14:56:27 +0530

    Day 12 programs added

---
+++ DAY 12/palindrome.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(int n) {
+    int original = n;
+    int rev = 0;
+
+    while (n > 0) {
+        int digit = n % 10;
+        rev = rev * 10 + digit;
+        n /= 10;
+    }
+
+    return original == rev;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (isPalindrome(n))
+        cout << "Palindrome Number";
+    else
+        cout << "Not Palindrome Number";
+
+    return 0;
+}
