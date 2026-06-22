449e4e33e864148bdad7166e247f612f1090b71e
chaudharypuja-creator <chaudharypuja292@gmail.com>
2026-06-22 20:42:12 +0530

    Day 22 program added

---
+++ DAY 22/character-frequency.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    int freq[256] = {0};
+
+    for (char ch : str) {
+        freq[(int)ch]++;
+    }
+
+    cout << "Character Frequencies:\n";
+
+    for (int i = 0; i < 256; i++) {
+        if (freq[i] > 0) {
+            cout << (char)i << " : " << freq[i] << endl;
+        }
+    }
+
+    return 0;
+}
---
+++ DAY 22/count_words.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main() {
+    string sentence;
+    
+    cout << "Enter a sentence: ";
+    getline(cin, sentence);
+
+    stringstream ss(sentence);
+    string word;
+    int count = 0;
+
+    while (ss >> word) {
+        count++;
+    }
+
+    cout << "Number of words = " << count;
+
+    return 0;
+}
